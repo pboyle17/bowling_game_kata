6d1c7a736e60f1403031818f7f85c1c05269cec6
Paul Boyle <pboyle17@gmail.com>
2016-05-10 21:02:27 -0500

    working prototype

--- game_logic/score_method.js
+++ game_logic/score_method.js
@@ -1,17 +1,28 @@
 var scoreKeeper = {
   score: function(arr){
     var sum=0;
-    var count = 0;
     var bonus = 0;
+
+    var lastRoll,twoRollsEarlier
+
     arr.forEach(function(roll){
-      if(count==2 && sum==10){
-        bonus+= roll;
+      if(lastRoll == 10){
+        bonus+=roll;
+      }
+      if(twoRollsEarlier == 10){
+        bonus+=roll;
+      }
+      else if(twoRollsEarlier+lastRoll==10){
+        bonus+=roll;
       }
       sum+=roll;
-      count++;
+      twoRollsEarlier = lastRoll
+      lastRoll = roll;
     });
+
     return sum+bonus;
   }
 }
+
 
 module.exports.scoreKeeper = scoreKeeper;
