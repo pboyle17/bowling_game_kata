b8717ebc360b559ab2514a475e4658c7b264094c
Paul Boyle <pboyle17@gmail.com>
2016-05-31 15:26:14 -0500

    some changes and stuffts

--- score_methods/score.js
+++ score_methods/score.js
@@ -1,54 +1,180 @@
 bowling = {
-  frame:1,
-  score:function(arr){
-    var sum = 0;
-    var lastRoll;
-    var twoRollsAgo;
-    var bonus=0;
-    var count=1
-
+  topOfFrame:true,
+  calculateScore:function(arr){
     arr.forEach(function(roll){
-      if(bowling.isLastRollStrike(lastRoll,twoRollsAgo) && bowling.frame != 10){
-        bonus+=roll;
-        bowling.frame++
-        count=1;
+      bowling.frames[bowling.currentFrame].pins+=roll;
+      if(bowling.frames[bowling.lastFrame()].strike==true){
+        bowling.frames[bowling.lastFrame()].pins+=roll;
       }
-      if(bowling.isTwoRollsAgoStrike(twoRollsAgo) && bowling.frame != 10){
-        bonus+=roll;
-        count++;
+      if(bowling.frames[bowling.twoFramesAgo()].strike==true){
+        bowling.frames[bowling.twoFramesAgo()].pins+=roll;
       }
-      if(bowling.isLastFrameASpare(lastRoll,twoRollsAgo)){
-        bonus+=roll;
+      if(bowling.topOfFrame==false){
+        bowling.nextFrame();
       }
-      if(count>2 && bowling.isLastRollStrike(lastRoll)==false && bowling.frame!=10){
-        bowling.frame++
-        count=1;
+      if(bowling.isStrike(roll)){
+        bowling.frames[bowling.currentFrame].strike = true;
+        bowling.nextFrame();
       }
-      // console.log('frame: ',bowling.frame);
-      sum+=roll;
-      twoRollsAgo = lastRoll
-      lastRoll = roll;
-      count++
-
+      bowling.topOfFrame=!bowling.topOfFrame;
     });
-    return sum + bonus;
+    return bowling.sum();
   },
-  isLastRollStrike:function(lastRoll,twoRollsAgo){
-    if(lastRoll==10 && twoRollsAgo!=0){
+  frames:{
+    one:{
+      pins:0,
+      strike:false,
+      spare:false
+    },
+    two:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    three:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    four:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    five:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    six:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    seven:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    eight:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    nine:{
+      pins:0,
+      strike:false,
+      spare:false,
+    },
+    ten:{
+      pins:0,
+      strike:false,
+      spare:false,
+    }
+  },
+  sum:function(){
+    return bowling.frames.one.pins + bowling.frames.two.pins + bowling.frames.three.pins + bowling.frames.four.pins + bowling.frames.five.pins + bowling.frames.six.pins + bowling.frames.seven.pins + bowling.frames.eight.pins + bowling.frames.nine.pins + bowling.frames.ten.pins;
+  },
+  currentFrame:'one',
+  nextFrame:function(){
+    switch(bowling.currentFrame){
+      case 'one':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'two';
+        break;
+      case 'two':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'three';
+        break;
+      case 'three':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'four';
+        break;
+      case 'four':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'five';
+        break;
+      case 'five':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'six';
+        break;
+      case 'six':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'seven';
+        break;
+      case 'seven':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'eight';
+        break;
+      case 'eight':
+        bowling.topOfFrame=true;
+        bowling.currenFrame = 'nine';
+        break;
+      case 'nine':
+        bowling.topOfFrame=true;
+        bowling.currentFrame = 'ten';
+        break;
+      case 'ten':
+        break;
+    }
+  },
+  lastFrame:function(){
+    switch(bowling.currentFrame){
+      case 'one':
+        return 'one'
+      case 'two':
+        return 'one'
+      case 'three':
+        return 'two';
+      case 'four':
+        return 'three';
+      case 'five':
+        return 'four';
+      case 'six':
+        return 'five';
+      case 'seven':
+        return 'six';
+      case 'eight':
+        return 'seven';
+      case 'nine':
+        return 'eight';
+      case 'ten':
+        return 'nine';
+    }
+  },
+  isStrike:function(roll){
+    if(roll==10 && bowling.topOfFrame==true){
       return true;
     } else {
       return false;
     }
   },
-  isTwoRollsAgoStrike:function(twoRollsAgo){
-    if(twoRollsAgo==10){
-      return true;
-    } else {
-      return false;
+  twoFramesAgo:function(){
+    switch(bowling.currentFrame){
+      case 'one':
+        return 'one'
+      case 'two':
+        return 'two'
+      case 'three':
+        return 'one';
+      case 'four':
+        return 'two';
+      case 'five':
+        return 'three';
+      case 'six':
+        return 'four';
+      case 'seven':
+        return 'five';
+      case 'eight':
+        return 'six';
+      case 'nine':
+        return 'seven';
+      case 'ten':
+        return 'eight';
     }
   },
-  isLastFrameASpare:function(lastRoll,twoRollsAgo){
-    if(lastRoll+twoRollsAgo ==10){
+  isSpare:function(){
+    if(bowling.frames[bowling.currentFrame].pins==10 && bowling.topOfFrame==false){
       return true;
     } else {
       return false;
--- test/score_spec.js
+++ test/score_spec.js
@@ -1,35 +1,534 @@
 var expect = require('chai').expect;
 
-describe('bowling',function(){
+describe('calculateScore',function(){
   var bowling = require('../score_methods/score').bowling;
+  var resetGame = function(){
+    bowling.frames={
+      one:{
+        pins:0,
+        strike:false,
+        spare:false
+      },
+      two:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      three:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      four:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      five:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      six:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      seven:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      eight:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      nine:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      ten:{
+        pins:0,
+        strike:false,
+        spare:false,
+      }
+    };
+    bowling.topOfFrame = true;
+    bowling.currentFrame = 'one';
+  };
 
   it('should return zero total score for a gutter game 20 rolls of zero',function(){
-    bowling.frame = 1;
-    expect(bowling.score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])).to.equal(0);
+    resetGame();
+    expect(bowling.calculateScore([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])).to.equal(0);
   });
 
   it('should return a score of 20 for twenty rolls of one',function(){
-    bowling.frame = 1;
-    expect(bowling.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).to.equal(20);
-  });
-
-  it('should return frame 10 for game of rolls of one',function(){
-    bowling.frame = 1;
-    bowling.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
-    expect(bowling.frame).to.equal(10);
+    resetGame();
+    expect(bowling.calculateScore([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).to.equal(20);
   });
 
   it('should return a score of 24 for a strike in the first frame followed by three pins,then for pins, followed by all misses',function(){
-    bowling.frame = 1;
-    expect(bowling.score([10,3,4,0,0,0,0,0,0,0,0,0,0,0])).to.equal(24);
+    resetGame();
+    expect(bowling.calculateScore([10,3,4,0,0,0,0,0,0,0,0,0,0,0])).to.equal(24);
   });
 
   it('should return 16 from arr [9,1,3,0,0,0,0,0,0]',function(){
-    bowling.frame = 1;
-    expect(bowling.score([9,1,3,0,0,0,0,0,0])).to.equal(16);
+    resetGame();
+    expect(bowling.calculateScore([9,1,3,0,0,0,0,0,0])).to.equal(16);
   });
   it('a perfect game should return 300',function(){
-    bowling.frame = 1;
-    expect(bowling.score([10,10,10,10,10,10,10,10,10,10,10,10])).to.equal(300);
+    resetGame()
+    expect(bowling.calculateScore([10,10,10,10,10,10,10,10,10,10,10,10])).to.equal(300);
+  });
+});
+
+describe('nextFrame',function(){
+  var bowling = require('../score_methods/score').bowling;
+
+  var resetGame = function(){
+    bowling.frames={
+      one:{
+        pins:0,
+        strike:false,
+        spare:false
+      },
+      two:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      three:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      four:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      five:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      six:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      seven:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      eight:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      nine:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      ten:{
+        pins:0,
+        strike:false,
+        spare:false,
+      }
+    };
+    bowling.topOfFrame = true;
+    bowling.currentFrame = 'one';
+  };
+
+  it('should return two when input one',function(){
+    bowling.currentFrame = 'one';
+    bowling.nextFrame();
+    expect(bowling.currentFrame).to.equal('two');
+  })
+  it('should return ten when current frame is ten',function(){
+    bowling.currentFrame = 'ten';
+    bowling.nextFrame();
+    expect(bowling.currentFrame).to.equal('ten');
+  })
+  it('should return top of frame true after nextFrame is called',function(){
+    resetGame();
+    bowling.topOfFrame = false;
+    bowling.nextFrame();
+
+    expect(bowling.topOfFrame).to.equal(true);
+  });
+});
+
+describe('lastFrame',function(){
+  var bowling = require('../score_methods/score').bowling;
+
+  var resetGame = function(){
+    bowling.frames={
+      one:{
+        pins:0,
+        strike:false,
+        spare:false
+      },
+      two:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      three:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      four:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      five:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      six:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      seven:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      eight:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      nine:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      ten:{
+        pins:0,
+        strike:false,
+        spare:false,
+      }
+    };
+    bowling.topOfFrame = true;
+    bowling.currentFrame = 'one';
+  };
+
+  it('should equal true',function(){
+    bowling.currentFrame='two';
+    expect(bowling.lastFrame()).to.equal('one');
+  });
+});
+
+describe('sum',function(){
+  var bowling = require('../score_methods/score').bowling;
+
+  var resetGame = function(){
+    bowling.frames={
+      one:{
+        pins:0,
+        strike:false,
+        spare:false
+      },
+      two:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      three:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      four:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      five:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      six:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      seven:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      eight:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      nine:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      ten:{
+        pins:0,
+        strike:false,
+        spare:false,
+      }
+    };
+    bowling.topOfFrame = true;
+    bowling.currentFrame = 'one';
+  };
+
+  it('should return the sum of all scores in all the frames',function(){
+    resetGame();
+    expect(bowling.sum()).to.equal(0);
+  });
+
+  it('should return 300 for frames of 30',function(){
+    resetGame();
+    bowling.frames.one.pins=30;
+    bowling.frames.two.pins=30;
+    bowling.frames.three.pins=30;
+    bowling.frames.four.pins=30;
+    bowling.frames.five.pins=30;
+    bowling.frames.six.pins=30;
+    bowling.frames.seven.pins=30;
+    bowling.frames.eight.pins=30;
+    bowling.frames.nine.pins=30;
+    bowling.frames.ten.pins=30;
+    expect(bowling.sum()).to.equal(300);
+  });
+});
+
+describe('isStrike',function(){
+  var bowling = require('../score_methods/score').bowling;
+
+  var resetGame = function(){
+    bowling.frames={
+      one:{
+        pins:0,
+        strike:false,
+        spare:false
+      },
+      two:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      three:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      four:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      five:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      six:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      seven:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      eight:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      nine:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      ten:{
+        pins:0,
+        strike:false,
+        spare:false,
+      }
+    };
+    bowling.topOfFrame = true;
+    bowling.currentFrame = 'one';
+  };
+
+  it('should return true if the roll was a strike and false if the roll isnt a strike',function(){
+    resetGame();
+    var roll = 10;
+    expect(bowling.isStrike(roll)).to.equal(true);
+  });
+
+  it('should return false if the roll was a 10 and its the bottom of the frame',function(){
+    bowling.frames={
+      one:0,
+      two:0,
+      three:0,
+      four:0,
+      five:0,
+      six:0,
+      seven:0,
+      eight:0,
+      nine:0,
+      ten:0
+    };
+    var roll = 10;
+    bowling.topOfFrame=false;
+    expect(bowling.isStrike(roll)).to.equal(false);
+  });
+});
+
+describe('twoRollsAgo',function(){
+  var bowling = require('../score_methods/score').bowling;
+
+  var resetGame = function(){
+    bowling.frames={
+      one:{
+        pins:0,
+        strike:false,
+        spare:false
+      },
+      two:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      three:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      four:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      five:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      six:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      seven:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      eight:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      nine:{
+        pins:0,
+        strike:false,
+        spare:false,
+      },
+      ten:{
+        pins:0,
+        strike:false,
+        spare:false,
+      }
+    };
+    bowling.topOfFrame = true;
+    bowling.currentFrame = 'one';
+  };
+
+  it('should return the string one if the current frame is three',function(){
+    resetGame();
+    bowling.currentFrame = 'three';
+    expect(bowling.twoFramesAgo()).to.equal('one');
+  });
+
+  describe('should return true if frame is a strike and false if not a strike',function(){
+    var bowling = require('../score_methods/score').bowling;
+    var resetGame = function(){
+      bowling.frames={
+        one:{
+          pins:0,
+          strike:false,
+          spare:false
+        },
+        two:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        three:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        four:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        five:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        six:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        seven:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        eight:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        nine:{
+          pins:0,
+          strike:false,
+          spare:false,
+        },
+        ten:{
+          pins:0,
+          strike:false,
+          spare:false,
+        }
+      };
+      bowling.topOfFrame = true;
+      bowling.currentFrame = 'one';
+    };
+
+    it('should return true if last two rolls add up to 10 and it is the bottom of the frame',function(){
+      bowling.frames.one.pins = 10;
+      bowling.topOfFrame=false;
+      expect(bowling.isSpare()).to.equal(true);
+    });
+    it('should return false if the last two rolls add up to 10 and it is the top of the frame',function(){
+      bowling.topOfFrame = true
+      bowling.frames.one.pins = 10;
+      expect(bowling.isSpare()).to.equal(false);
+    })
   });
 });
